650c936cd4e418e8d7a25d535118528e0c1aa720
pulkit-0715 <singhpulkit1506@gmail.com>
2026-05-24 03:02:08 +0530

    Python automation file

---
+++ automation.py
@@ -0,0 +1,16 @@
+import subprocess
+
+commands = [
+    ["node", "generate-json.js"],
+    ["git", "add", "."],
+    ["git", "commit", "-m", "Python automation file"],
+    ["git", "push", "origin", "main"]
+]
+
+for command in commands:
+    print(f"Running: {' '.join(command)}")
+    result = subprocess.run(command)
+
+    if result.returncode != 0:
+        print(f"Error while running: {' '.join(command)}")
+        break
